613bfd28ba3caa4657c3414cf652f74d163a2304
ThienDuc <59289834+thienduc12311@users.noreply.github.com>
2025-10-19 16:17:25 -0500

    Update battery_status.c

--- config/widgets/battery_status.c
+++ config/widgets/battery_status.c
@@ -6,12 +6,12 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/bluetooth/services/bas.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
+#include <zmk/battery.h>
 #include "battery_status.h"
 #include <zmk/usb.h>
 #include <zmk/events/usb_conn_state_changed.h>
@@ -41,21 +41,52 @@ LV_IMG_DECLARE(batt_0);
 LV_IMG_DECLARE(batt_0_chg);
 
 static void set_battery_symbol(lv_obj_t *icon, struct battery_status_state state) {
-    uint8_t level = state.level;
-
 #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-    if (level > 95) {
-        lv_img_set_src(icon, state.usb_present ? &batt_100_chg : &batt_100);
-    } else if (level > 74) {
-        lv_img_set_src(icon, state.usb_present ? &batt_75_chg : &batt_75);
-    } else if (level > 49) {
-        lv_img_set_src(icon, state.usb_present ? &batt_50_chg : &batt_50);
-    } else if (level > 24) {
-        lv_img_set_src(icon, state.usb_present ? &batt_25_chg : &batt_25);
+    static uint8_t stage_prev = 255;
+    static bool usb_prev = false;
+
+    uint8_t level = state.level;
+    bool usb_present = state.usb_present;
+    uint8_t stage;
+
+    if (level > 87) {
+        stage = 5;
+    } else if (level > 62) {
+        stage = 4;
+    } else if (level > 37) {
+        stage = 3;
+    } else if (level > 12) {
+        stage = 2;
     } else if (level > 5) {
-        lv_img_set_src(icon, state.usb_present ? &batt_5_chg : &batt_5);
+        stage = 1;
     } else {
-        lv_img_set_src(icon, state.usb_present ? &batt_0_chg : &batt_0);
+        stage = 0;
+    }
+
+    // check if there is a change requiring an update
+    if (usb_present != usb_prev || stage != stage_prev) {
+        switch (stage) {
+        case 5:
+            lv_img_set_src(icon, usb_present ? &batt_100_chg : &batt_100);
+            break;
+        case 4:
+            lv_img_set_src(icon, usb_present ? &batt_75_chg : &batt_75);
+            break;
+        case 3:
+            lv_img_set_src(icon, usb_present ? &batt_50_chg : &batt_50);
+            break;
+        case 2:
+            lv_img_set_src(icon, usb_present ? &batt_25_chg : &batt_25);
+            break;
+        case 1:
+            lv_img_set_src(icon, usb_present ? &batt_5_chg : &batt_5);
+            break;
+        default:
+            lv_img_set_src(icon, usb_present ? &batt_0_chg : &batt_0);
+            break;
+        }
+        usb_prev = usb_present;
+        stage_prev = stage;
     }
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
 }
@@ -66,8 +97,10 @@ void battery_status_update_cb(struct battery_status_state state) {
 }
 
 static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
-    return (struct battery_status_state) {
-        .level = bt_bas_get_battery_level(),
+    const struct zmk_battery_state_changed *ev = as_zmk_battery_state_changed(eh);
+
+    return (struct battery_status_state){
+        .level = (ev != NULL) ? ev->state_of_charge : zmk_battery_state_of_charge(),
 #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
         .usb_present = zmk_usb_is_powered(),
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
